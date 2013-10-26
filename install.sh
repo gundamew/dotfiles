098524f1cac3460ff68b2708f2a7dcbfb821aab9
gundamew <gundamew@gmail.com>
2013-10-26 15:17:03 +0800

    [Modify] Adjust Bash settings

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 if [ -d $HOME/dotfiles/bash ]; then
+  ln -s $HOME/dotfiles/bash/exports $HOME/.exports
   ln -s $HOME/dotfiles/bash/bash_profile $HOME/.bash_profile
   ln -s $HOME/dotfiles/bash/bashrc $HOME/.bashrc
   ln -s $HOME/dotfiles/bash/bash_prompt $HOME/.bash_prompt
-  ln -s $HOME/dotfiles/bash/bash_function $HOME/.bash_function
-  ln -s $HOME/dotfiles/bash/bash_alias $HOME/.bash_alias
+  ln -s $HOME/dotfiles/bash/functions $HOME/.functions
+  ln -s $HOME/dotfiles/bash/aliases $HOME/.aliases
   ln -s $HOME/dotfiles/bash/screenrc $HOME/.screenrc
 else
   echo 'The dotfiles should clone into your home directory.'
