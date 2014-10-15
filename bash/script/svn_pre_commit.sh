12ca4ee1dbaf3226f1e8215ef5d63bee5b60b2f3
gundamew <gundamew@gmail.com>
2014-10-15 16:07:32 +0800

    [Modify] Change the way to store and display messages

--- bash/script/svn_pre_commit.sh
+++ bash/script/svn_pre_commit.sh
@@ -14,27 +14,45 @@
 source ${GIT_LOCAL}/dotfiles/bash/functions_svn
 
 function svn_php_lint() {
-  svn_modified_files_php | while read file; do
-    php_lint_result=`php -l $file 2>&1 | grep -i 'parse error'`
-    if [ -z $php_lint_result ]; then
-      echo "No syntax errors detected"
+  declare -a php_lint_result
+  while read file; do
+    php_lint_error=`php -l $file 2>&1 | grep -i 'parse error'`
+
+    if [ -n "$php_lint_error" ]; then
+      php_lint_result+=("$php_lint_error")
     fi
-  done
+  done < <(svn_modified_files_php)
+
+  if [ ${#php_lint_result[@]} -gt 0 ]; then
+    for ((i = 0; i < ${#php_lint_result[@]}; i++)); do
+      echo "${php_lint_result[$i]}"
+    done
+  else
+    echo "No syntax errors detected"
+  fi
 }
 
 function svn_crlf_checker() {
-  svn_modified_files | while read file; do
+  declare -a crlf_check_result
+  while read file; do
     crlf_amount=`grep -P '\r\n$' $file | wc -l`
+
     if [ $crlf_amount -gt 0 ]; then
-      echo "$file: $crlf_amount"
-    else
-      echo "No CRLF detected"
+      crlf_check_result+=("$file: $crlf_amount")
     fi
-  done
+  done < <(svn_modified_files)
+
+  if [ ${#crlf_check_result[@]} -gt 0 ]; then
+    for ((i = 0; i < ${#crlf_check_result[@]}; i++)); do
+      echo "${crlf_check_result[$i]}"
+    done
+  else
+    echo "No CRLF detected"
+  fi
 }
 
 echo "> PHP syntax error:"
 svn_php_lint
 echo -ne "\n"
-echo "> CRLF:"
+echo "> CRLF detected:"
 svn_crlf_checker
