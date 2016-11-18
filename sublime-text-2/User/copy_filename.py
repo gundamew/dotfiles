a0eba8570a860e42c04a33231fa789552c75e501
bschen <bschen@panmedia.asia>
2016-11-18 14:56:57 +0800

    I don't really need so many dotfiles

--- sublime-text-2/User/copy_filename.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import sublime, sublime_plugin, os
-
-class CopyFilenameCommand(sublime_plugin.TextCommand):
-   def run(self, edit):
-      sublime.set_clipboard(os.path.basename(self.view.file_name()))
-      sublime.status_message("Copied file name")
