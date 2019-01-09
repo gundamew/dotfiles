6bddcabbee50652519ebd717fbe17921860fb628
Bing-Sheng Chen <gundamew@gmail.com>
2019-01-09 12:13:51 +0800

    feat(config): Add Git commit message template
    Use a template to unify the format of commit messages.
    
    * https://chris.beams.io/posts/git-commit/
    * https://robots.thoughtbot.com/better-commit-messages-with-a-gitmessage-template
    * https://codeinthehole.com/tips/a-useful-template-for-commit-messages/
    * https://gist.github.com/adeekshith/cd4c95a064977cdc6c50

--- init.sh
+++ init.sh
@@ -13,6 +13,7 @@ ln -s ~/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/dotfiles/bashrc ~/.bashrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
+ln -s ~/dotfiles/gitmessage ~/.gitmessage
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/ackrc ~/.ackrc
 
